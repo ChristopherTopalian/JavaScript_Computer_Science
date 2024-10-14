8440f644509c8da253c227837dfb4de636b6ade9
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-14 11:24:46 -0400

    Add files via upload

---
+++ JavaScript_Computer_Science/208_PURE_Password_Generator_Specify_How_Many_Characters_without_Validation.js
@@ -0,0 +1,46 @@
+javascript:(
+/* Password_Generator_Specify_How_Many_Characters_without_Validation.js */
+function()
+{
+    function generatePassword(length)
+    {
+        let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?/{}[]";
+        let password = "";
+
+        // generate random characters for password
+        for (let x = 0; x < length; x++)
+        {
+            let randomIndex = Math.floor(Math.random() * characters.length);
+            password += characters.charAt(randomIndex);
+        }
+
+        return password;
+    }
+
+    function makePassword()
+    {
+        let passwordLength = prompt("Enter how many characters");
+
+        if (passwordLength !== null)
+        {
+            let parsedLength = parseInt(passwordLength);
+
+            // generate password with the specified length
+            let generatedPassword = generatePassword(parsedLength);
+
+            console.log("Password is " + generatedPassword);
+
+            alert("Password is " + generatedPassword);
+        }
+    }
+
+    makePassword();
+
+}());
+
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024 */
+/* https://github.com/ChristopherTopalian */
+/* https://github.com/ChristopherAndrewTopalian */
+/* https://sites.google.com/view/CollegeOfScripting */
+
